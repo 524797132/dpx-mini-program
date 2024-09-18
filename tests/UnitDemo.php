80be2948e0a4b14287a050eaf0c2075195864b56
zhouwenming <524797132@qq.com>
2024-09-18 16:36:51 +0800

    first   commit

--- tests/UnitDemo.php
+++ tests/UnitDemo.php
@@ -1,6 +1,10 @@
 <?php
 namespace Zhouwenming\DpxMiniProgram\Tests\UserUnit;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+use GuzzleHttp\Client;
 
+require_once __DIR__ . '/../vendor/autoload.php';
+$client = new Client();
+$response = $client->request('GET', 'https://api.dpxkjw.com/api/tencent/goods-warehouse-xml');
+print_r($response->getBody()->getContents());
 UserCase::testFindUserByCondition();
