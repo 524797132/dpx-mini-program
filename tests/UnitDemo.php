b3484a2118e7235ae9ed3dc69000d5ca18363b07
zhouwenming <524797132@qq.com>
2024-09-18 16:20:40 +0800

    first   commit

---
+++ tests/UnitDemo.php
@@ -0,0 +1,6 @@
+<?php
+namespace Zhouwenming\DpxMiniProgram\Tests\UserUnit;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+UserCase::testFindUserByCondition();
--- tests/UserUnit/UserCase.php
+++ tests/UserUnit/UserCase.php
@@ -1,6 +1,14 @@
 <?php
+namespace Zhouwenming\DpxMiniProgram\Tests\UserUnit;
 require_once __DIR__ . '/../../vendor/autoload.php';
 use Zhouwenming\DpxMiniProgram\Module\UserServer\User;
 
-$user = new User();
-$user->findUserByCondition();
+class UserCase {
+    public static function testFindUserByCondition()
+    {
+        $user = new User();
+        $user->findUserByCondition();
+    }
+}
+
+
